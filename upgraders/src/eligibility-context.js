9d15eebec90fea74f617c20795f3a1d8bb0caa67
Jorge Corradi <jorge-luiz.corradi.ctr@plusgrid.com>
2023-05-11 15:24:36 -0300

    Cleaning up

--- upgraders/src/eligibility-context.js
+++ upgraders/src/eligibility-context.js
@@ -1,5 +1,5 @@
 import { gql, useQuery } from "@apollo/client";
-import React, { createContext, useContext, useEffect, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const EligibilityContext = createContext();
 
