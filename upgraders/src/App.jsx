4758b74f85f298acd549602044788d8d9582a0ad
Sina Rafiei <msina.raf@gmail.com>
2023-05-11 08:21:39 -0400

    finished the other sections

--- upgraders/src/App.jsx
+++ upgraders/src/App.jsx
@@ -1,5 +1,8 @@
 import { HeroSection } from "./components/hero-section";
 import { Navbar } from "./components/navbar";
+import { NumberSection } from "./components/numbers";
+import { OtherSection } from "./components/otherSections";
+import { PartnersSection } from "./components/partners";
 import { EligibilityProvider } from "./eligibility-context";
 
 function App() {
@@ -8,7 +11,11 @@ function App() {
       <div className="text-lg text-gray-600 bg-blue-900">
         <Navbar></Navbar>
         <HeroSection />
-        <div className="h-screen bg-white"></div>
+        <div className="bg-white">
+          <NumberSection />
+          <PartnersSection />
+          <OtherSection />
+        </div>
       </div>
     </EligibilityProvider>
   );
---
+++ upgraders/src/components/otherSections.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import review from "../assets/img/review.jpg";
+import why from "../assets/img/why.jpg";
+import description from "../assets/img/description.jpg";
+import footer from "../assets/img/footer.jpg";
+
+export const OtherSection = () => {
+  return (
+    <div>
+        <img src={review} />
+        <img src={why} />
+        <img src={description} />
+        <img src={footer} />
+    </div>
+  );
+};
