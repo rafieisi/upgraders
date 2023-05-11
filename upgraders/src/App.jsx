8429245ac9bdb7914a4e798f3f06b5fd3af62444
Jorge Corradi <jorge-luiz.corradi.ctr@plusgrid.com>
2023-05-11 14:59:42 -0300

    Introducing apollo

--- upgraders/src/App.jsx
+++ upgraders/src/App.jsx
@@ -1,23 +1,27 @@
+import { ApolloProvider } from "@apollo/client";
 import { HeroSection } from "./components/hero-section";
 import { Navbar } from "./components/navbar";
 import { NumberSection } from "./components/numbers";
 import { OtherSection } from "./components/otherSections";
 import { PartnersSection } from "./components/partners";
 import { EligibilityProvider } from "./eligibility-context";
+import { apolloClient } from "./apolloClient";
 
 function App() {
   return (
-    <EligibilityProvider>
-      <div className="text-lg text-gray-600 bg-blue-900">
-        <Navbar></Navbar>
-        <HeroSection />
-        <div className="bg-white">
-          <NumberSection />
-          <PartnersSection />
-          <OtherSection />
+    <ApolloProvider client={apolloClient}>
+      <EligibilityProvider>
+        <div className="text-lg text-gray-600 bg-blue-900">
+          <Navbar></Navbar>
+          <HeroSection />
+          <div className="bg-white">
+            <NumberSection />
+            <PartnersSection />
+            <OtherSection />
+          </div>
         </div>
-      </div>
-    </EligibilityProvider>
+      </EligibilityProvider>
+    </ApolloProvider>
   );
 }
 
--- upgraders/src/eligibility-context.js
+++ upgraders/src/eligibility-context.js
@@ -1,14 +1,36 @@
-import React, { createContext, useContext, useState } from "react";
+import { gql, useQuery } from "@apollo/client";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 const EligibilityContext = createContext();
 
+const GET_OFFER_URL = gql`
+  query getOfferUrl($pid: String!, $pnr: String!, $lastname: String!) {
+    getOfferUrl(pid: $pid, pnr: $pnr, lastname: $lastname) {
+      offerUrl
+    }
+  }
+`;
+
 const useEligibility = () => {
-  const [pid, setPid] = useState();
+  const [pid, setPid] = useState("5stlBZRGQF");
   const [lastName, setLastName] = useState();
   const [pnr, setPnr] = useState();
+  const [offerUrl, setOfferUrl] = useState("");
+
+  const { error, loading } = useQuery(GET_OFFER_URL, {
+    variables: {
+      pid: pid,
+      pnr: pnr,
+      lastname: lastName,
+    },
+    onCompleted: (data) => {
+      setOfferUrl(data.getOfferUrl.offerUrl);
+    },
+    skip: !pid || !pnr || !lastName,
+  });
 
   return {
-    state: { pid, lastName, pnr },
+    state: { pid, lastName, pnr, loading, error, offerUrl },
     actions: {
       setPid,
       setLastName,
